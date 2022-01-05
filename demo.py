143231eae9272392dc95582b02b282432bfaf28d
MEGANATHAN-IN <81283498+MEGANATHAN-IN@users.noreply.github.com>
2022-01-05 13:59:56 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,8 +2,7 @@ from flask import Flask,request,send_from_directory
 import os
 app = Flask(__name__)
 @app.route('/', methods=['GET','POST'])
-
-def home():
+def nltkmodule(text):
     #print()
     from nltk.tokenize import word_tokenize
     from nltk.tokenize import RegexpTokenizer
@@ -66,6 +65,36 @@ def home():
                 badwords.append(word) 
         result=[word for word in lemmatized_words if word in badwords]
     return " hello mega"
+    
+def home():
+
+    teststr="hello! Mr Barath Nithish you are a, \"fuc.ker\" bastard "
+    li=list(teststr.split())
+    print(li)
+    li2=[]
+    string=''
+    for i in li:
+        li3=[]
+        for j in i:
+            if j.isalpha():
+               j=j.lower()
+               print(j,sep=' ')
+               li3.append(j)
+        li3="".join(li3)
+        li2.append(li3)
+    print(li2)
+    sentence=" ".join(li2)
+    badwords=[]
+
+    with open('D:/python/Words.txt','r',encoding="utf8") as file:
+   
+        for line in file:
+       
+            # reading each word        
+            for word in line.split():   
+                badwords.append(word) 
+        result=[word for word in sentence if word in badwords]
+    return " ".join(result)
 
     
     
